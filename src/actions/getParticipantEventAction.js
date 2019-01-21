e800db1913f89519a6fcb39e7d149c2a5df7aa62
ThibWill <thibault.willer@gmail.com>
2019-01-21 21:19:02 +0100

    merge master

--- src/actions/getParticipantEventAction.js
+++ src/actions/getParticipantEventAction.js
@@ -1,6 +1,6 @@
 import { createAction } from 'redux-actions';
 import axios from '../helpers/axios';
-// import { getAsyncStorageItem } from '../helpers/common';
+import { getAsyncStorageItem } from '../helpers/common';
 
 export const GET_PARTICIPANT_REQUEST = 'GET_PARTICIPANT_REQUEST';
 export const GET_PARTICIPANT_SUCCESS = 'GET_PARTICIPANT_SUCCESS';
@@ -12,8 +12,8 @@ export const getParticipantError = createAction(GET_PARTICIPANT_ERROR);
 
 export default ({ idEvent }) => async (dispatch) => {
   dispatch(getParticipantRequest());
-  // const idUser = await getAsyncStorageItem.getItem('idUser');
-  return axios.get(`event?idEvent=${idEvent}`)
+  const idUser = await getAsyncStorageItem('idUser');
+  return axios.get(`event?idEvent=${idEvent}&idUser=${idUser}`)
     .then(res => dispatch(getParticipantSuccess(res)))
     .catch(err => dispatch(getParticipantError(err)));
 };
